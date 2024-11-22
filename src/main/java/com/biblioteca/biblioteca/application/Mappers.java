f3ca660bcbdd7e967df524653232047d1cb12a45
Fernando Reginato Panissa <fernandoreginato3@gmail.com>
2024-11-22 01:45:24 -0300

    terminando de fazer Usuario

---
+++ src/main/java/com/biblioteca/biblioteca/application/Mappers.java
@@ -0,0 +1,15 @@
+package com.biblioteca.biblioteca.application;
+
+import org.mapstruct.Mapper;
+import com.biblioteca.biblioteca.domain.dto.UsuarioDTO;
+import com.biblioteca.biblioteca.domain.entity.Usuario;
+
+@Mapper(componentModel = "spring")
+public interface Mappers {
+    
+    UsuarioDTO UsuariotoDto(Usuario usuario);
+    Usuario UsuarioDTOtoEntity(UsuarioDTO usuarioDTO);
+
+
+
+}
---
+++ src/main/java/com/biblioteca/biblioteca/domain/service/IUsuarioService.java
@@ -0,0 +1,20 @@
+package com.biblioteca.biblioteca.domain.service;
+
+import java.util.List;
+
+import com.biblioteca.biblioteca.domain.dto.UsuarioDTO;
+
+public interface IUsuarioService {
+    
+    UsuarioDTO buscarPorId(Long id);
+
+    UsuarioDTO buscarPorEmail(String email);
+
+    List<UsuarioDTO> listarTodosUsuarios();
+   
+    UsuarioDTO cadastrarUsuario(UsuarioDTO usuarioDTO);
+
+    UsuarioDTO atualizarUsuario(Long id, UsuarioDTO usuarioAtualizado);
+
+    void removerUsuario(Long id);
+}
